649c2133a7eb95b4edb344f3256a89439d986384
Will Cook <will@octave.nz>
2024-10-09 11:19:39 +1300

    added a light and enviroment to start lighting the shirt

--- src/Canvas.js
+++ src/Canvas.js
@@ -2,22 +2,27 @@ import { Canvas } from "@react-three/fiber";
 import {
     useGLTF,
     OrbitControls,
-    Center
+    Center,
+    Environment
 } from "@react-three/drei";
 import { useRef } from "react";
 
 export const App = ({ position=[-1,0,2.5], fov = 25 }) => (
-   <Canvas eventSource={ document.getElementById('root')}
-           eventPrefix={"client"}
-           camera={{ position, fov }}>
-       <Center>
-           <Shirt />
-       </Center>
-       <OrbitControls />
-   </Canvas>
+    <Canvas eventSource={document.getElementById('root')}
+            eventPrefix={"client"}
+            camera={{position, fov}}>
+
+        <ambientLight intensity={ 0.5 }/>
+
+        <Environment preset={ "city" } />
+        <Center>
+            <Shirt/>
+        </Center>
+        <OrbitControls/>
+    </Canvas>
 );
 
-function Shirt (props) {
+function Shirt(props) {
     const { nodes, materials } = useGLTF('/shirt_baked_collapsed.glb')
 
     return (
