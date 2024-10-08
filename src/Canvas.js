368a8c92a2fc2d64328a5c281634d61dfce080d0
Will Cook <will@octave.nz>
2024-10-09 11:27:49 +1300

    Added backdrop and some initial lights to the scene

--- src/Canvas.js
+++ src/Canvas.js
@@ -1,22 +1,27 @@
 import { Canvas } from "@react-three/fiber";
 import {
+    AccumulativeShadows,
     useGLTF,
     OrbitControls,
     Center,
-    Environment
+    Environment,
+    RandomizedLight
 } from "@react-three/drei";
 import { useRef } from "react";
 
 export const App = ({ position=[-1,0,2.5], fov = 25 }) => (
     <Canvas eventSource={document.getElementById('root')}
             eventPrefix={"client"}
-            camera={{position, fov}}>
+            camera={{position, fov}}
+            shadows
+    >
 
         <ambientLight intensity={ 0.5 }/>
 
         <Environment preset={ "city" } />
         <Center>
             <Shirt/>
+            <Backdrop />
         </Center>
         <OrbitControls/>
     </Canvas>
@@ -34,6 +39,36 @@ function Shirt(props) {
           {...props}
           dispose={null}></mesh>
       )
+}
+
+function Backdrop() {
+    const shadows = useRef()
+
+    return (
+        <AccumulativeShadows
+            ref={shadows}
+            temporal
+            frames={60}
+            alphaTest={0.85}
+            scale={10}
+            rotation={[Math.PI / 2, 0, 0]}
+            position={[0, 0, -0.14]}>
+            <RandomizedLight
+                amount={4}
+                radius={9}
+                intensity={0.55}
+                ambient={0.25}
+                position={[5, 5, -10]}
+            />
+            <RandomizedLight
+                amount={4}
+                radius={5}
+                intensity={0.25}
+                ambient={0.55}
+                position={[-5, 5, -9]}
+            />
+        </AccumulativeShadows>
+    )
 }
 
 useGLTF.preload('/shirt_baked_collapsed.glb')
